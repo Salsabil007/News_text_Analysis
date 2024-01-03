6f3d3516661cdbfbfb3827ab748dc92b4f954ca0
Salsabil007 <salsabilarabishusmi007@gmail.com>
2024-01-03 09:18:06 -0600

    non-binary testing

--- preprocess/test_prediction_title.py
+++ preprocess/test_prediction_title.py
@@ -19,10 +19,31 @@ text['G'] = G
 a1 = text[text['title_pred_bin'] == 'C']
 a2 = text[text['title_pred_bin'] == 'U']
 
+print("proportion of title with uncertainty ",len(a2)/len(text))
+
 
 a1f = a1[a1['G'] == "Female"]
 a2f = a2[a2['G'] == "Female"]
+a2m = a2[a2['G'] == "Male"]
 
-print(len(a1f)/len(a1)," ",len(a2f)/len(a2))
+print("propor of female in certain title group ",len(a1f)/len(a1)," prop of female in certain group ",len(a2f)/len(a2))
 
-print(len(text[text['G'] == "Female"])/len(text))
+print("proportion of female in the entire set",len(text[text['G'] == "Female"])/len(text))
+
+
+nf = text[text['G'] == "Female"]
+nm = text[text['G'] == "Male"]
+
+nfu = nf[nf['title_pred_bin'] == 'U']
+nmu = nm[nm['title_pred_bin'] == 'U']
+
+
+print(len(text)," male: ",len(nm)," female: ",len(nf))
+print(nf['title_pred_nobin'].value_counts().reset_index())
+print(nm['title_pred_nobin'].value_counts().reset_index())
+
+
+print("c ",1386/1540," E ",144/len(nfu)," I ",3/len(nfu)," U ",3/len(nfu)," N ",3/len(nfu)," D ",1/len(nfu))
+print("c ",3891/4316," E ",372/len(nmu)," I ",10/len(nmu)," U ",4/len(nmu)," N ",31/len(nmu)," D ",8/len(nmu))
+
+print(a2['Title'])
--- preprocess/testing_pred.py
+++ preprocess/testing_pred.py
@@ -4,7 +4,7 @@ import ast
 
 
 text = pd.read_csv("pred.csv", converters={'sentence': ast.literal_eval,'sent_pred': ast.literal_eval})
-#print(len(text))
+print(len(text))
 #print(len(text.iloc[0]['sentence']))
 #print(len(text.iloc[0]['sent_pred']))
 
@@ -30,13 +30,44 @@ for ind in text.index:
         G.append("Male")
     elif text['gender'][ind] == 'female' or text['gender'][ind] == 'F':
         G.append("Female")
-     
 
-
-    
 text['total_cnt'] = total_cnt
 text['uncertain'] = uncertain
 text['G'] = G
+     
+C,E,N,I,D,U = [],[],[],[],[],[]
+for ind in text.index:
+    
+    c,e,n,ii,d,u = 0,0,0,0,0,0
+    for i in text['sent_pred_nobin'][ind]:
+        if i == 'U':
+            u += 1
+        elif i == 'C':
+            c += 1
+        elif i == 'E':
+            e += 1
+        elif i == 'D':
+            d += 1
+        elif i == 'I':
+            ii += 1
+        elif i == 'N':
+            n += 1
+    C.append(c)
+    U.append(u)
+    E.append(e)
+    N.append(n)
+    D.append(d)
+    I.append(ii)
+
+    
+
+    
+text['C'] = C
+text['U'] = U
+text['D'] = D
+text['E'] = E
+text['I'] = I
+text['N'] = N
 
 
 
@@ -50,17 +81,46 @@ text['prop'] = text['uncertain']/text['total_cnt']
 
 text = text[text['total_cnt'] > 5]
 text = text[text['total_cnt'] < 100]
+print(len(text))
 
 f = text[text['G'] == "Female"]
 m = text[text['G'] == "Male"]
-m = m.sample(n = len(f), replace = False, random_state=0)
+#m = m.sample(n = len(f), replace = False, random_state=0)
 
 print(f['prop'].mean()," ", m['prop'].mean())
+print(f['prop'].median()," ", m['prop'].median())
 
 #print(f['total_cnt'].mean()," ", m['total_cnt'].mean())
 print(len(f)," ",len(m))
 
+a = text[text['uncertain'] == 0]
+print("proportion of article with certainty ",len(a)/len(text))
+
+af = a[a['G'] == "Female"]
+print("proportion of female in certain group ",len(af)/len(a))
+
+a = text[text['uncertain'] > 0]
+af = a[a['G'] == "Female"]
+print("proportion of female in uncertain group ",len(af)/len(a))
+
+f = f[f['uncertain'] > 0]
+m = m[m['uncertain'] > 0]
+print(len(f)," ",len(m))
+
+f['U'] = f['U']/f['uncertain']
+f['D'] = f['D']/f['uncertain']
+f['N'] = f['N']/f['uncertain']
+f['E'] = f['E']/f['uncertain']
+f['I'] = f['I']/f['uncertain']
 
 
+m['U'] = m['U']/m['uncertain']
+m['D'] = m['D']/m['uncertain']
+m['N'] = m['N']/m['uncertain']
+m['E'] = m['E']/m['uncertain']
+m['I'] = m['I']/m['uncertain']
 
+
+print("male U ",m['U'].mean()," E ",m['E'].mean(), " D ",m['D'].mean()," N ",m['N'].mean()," I ",m['I'].mean())
+print("female U ",f['U'].mean()," E ",f['E'].mean(), " D ",f['D'].mean()," N ",f['N'].mean()," I ",f['I'].mean())
 
