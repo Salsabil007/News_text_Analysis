c681328d96bd8c5f0b5293113420e1012225117d
Salsabil007 <salsabilarabishusmi007@gmail.com>
2024-01-03 09:15:39 -0600

    sentence level non-binary

--- preprocess/run_luci.py
+++ preprocess/run_luci.py
@@ -4,19 +4,29 @@ import numpy as np
 import ast
 
 cls = Classifier(granularity = "sentence", binary = True)
-text = pd.read_csv("interim.csv", converters={'sentence': ast.literal_eval})
-text = text.head(5)
+cls2 = Classifier(granularity = "sentence")
+text = pd.read_csv("interim_dec29.csv", converters={'sentence': ast.literal_eval})
+#text = text.head(5)
 
 pred = []
 
+pred2 = []
+
 for ind in text.index:
     lst = []
+    lst2 = []
     for i in text['sentence'][ind]:
         a = cls.predict(i)
         lst.append(a)
+
+        a2 = cls2.predict(i)
+        lst2.append(a2)
+
     pred.append(lst)
+    pred2.append(lst2)
 
 text['sent_pred'] = pred
+text['sent_pred_nobin'] = pred2
 text.to_csv("pred.csv", index = False)
 
 
